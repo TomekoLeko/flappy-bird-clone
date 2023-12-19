ca82eee087346e8b6a01155824161933f9d5d9c3
Maciej Machniewski <67371062+mmach1988@users.noreply.github.com>
2023-12-19 15:39:09 +0100

    Dodane rury

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import Phaser from "phaser";
 
 const config = {
@@ -8,21 +7,57 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
+      debug: true
+      // gravity: { y: 50 }
     }
   },
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
 new Phaser.Game(config);
+let bird = null;
+let pipeDown = null;
+let pipeUp = null;
+let distance = 200;
+const VELOCITY = 200;
+const initialBirdPosition = {
+  x: 100,
+  y: 45 
+}
+
 
 function preload () {
   this.load.image('sky', 'assets/sky.png');
+  this.load.image('dude5', 'assets/dude.png'); 
+  this.load.image('pipe', 'assets/pipe.png'); 
 }
 
 function create () {
   this.add.image(400, 300, 'sky');
+  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'dude').setOrigin(0, 0);
+  this.input.keyboard.on('keydown_SPACE', flap);
+  bird.setGravityY(50);
+
+  pipeUp = this.physics.add.sprite(500, 200, 'pipe').setOrigin(0, 1);
+  pipeDown = this.physics.add.sprite(500, 400, 'pipe').setOrigin(0, 0);
+
+}
+
+function update (time, delta) {
+  if (bird.y > (600 - bird.body.height) || bird.y < 0) {
+    resetGame();
+  }
+}
+
+function resetGame() {
+  bird.y = initialBirdPosition.y; 
+  bird.body.velocity.y = 0;
+}
+
+function flap() {
+  bird.body.velocity.y = -100;
 }
